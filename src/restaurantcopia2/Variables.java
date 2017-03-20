35d2f55a18dce2bf3398c0eb7485a2a0dcb4a003
jerje <jerje@DESKTOP-E3BHB3L>
2017-03-20 17:59:00 +0100

    Modularitza't case 3 i creades noves class

---
+++ src/restaurantcopia2/Variables.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurantcopia2;
+
+/**
+ *
+ * @author jerje
+ */
+public class Variables {
+        int iOpcioPrincipal;//Opcio menú
+        int iOpcioReserva;//Opcio reserva
+        int iOpcioSolicitud;//Opcio Sol·licitud Reserva
+        int iOpcioModificar;//Opcio Modificar Reserva
+        int iUsuari = 0;//Posicio usuari admin/usu
+        int iGestio = 0;//Opcions administrador
+        int iMes = 0;//Mes per cercar reserves administrador
+        int iNumeroReserva = 0;//Numero de reserva 
+        int c = 0;//Posicio Base de dades reserva
+        int iTelefon;//Variable numero telefon
+        int iMesReserva = 0;//Variable Mes reserva
+        int iDiaReserva = 0;//Variable dia reserva
+        int iComensalsR =0;//Variable comensals reserva
+        
+        String nTelefonReserva = "Introdueixi el numero de telefon amb el que va fer la reserva:\n -> "; //Text demanar Telefon reserva(Visualitzat/Modificar)
+
+        
+}
