190e70ec13fe51b63775bcc954e7d2e0e3497177
jerje <jerje@DESKTOP-E3BHB3L>
2017-03-20 17:58:48 +0100

    Modularitza't case 3 i creades noves class

---
+++ src/restaurantcopia2/Menus.java
@@ -0,0 +1,49 @@
+
+package restaurantcopia2;
+
+class Menus {
+        String[] MenuPrincipal = {"------------\n RESTAURANT \n------------\n "
+            + "[1] Iniciar Sessio\n "
+            + "[2] Registrar-se\n "
+            + "[3] Recuperar Contrasenya\n "
+            + "[4] Tancar aplicació\n "};
+        
+        String[] MenuReserves = {"---------------\n MENU RESERVES\n---------------\n "
+            + "[1] Sol·licitar reserva\n "
+            + "[2] Cancel·lar reserva\n "
+            + "[3] Buscar reserva\n "
+            + "[4] Modificar reserva\n "
+            + "[5] Tancar sessió\n "};
+        
+        String[] MenuOpcionsModificarReserva = {"Indique que voleu modificar:\n "
+            + "[1] Nom\n "
+            + "[2] Nº de comensals\n "
+            + "[3] Dia\n "
+            + "[4] Mes\n "
+            + "[5] Tornar al menú\n "};
+        
+        String[] MenuGestioReserves = {"--------------------\n GESTIO DE RESERVES \n--------------------\n "
+            + "[1] Reserves per mes\n "
+            + "[2] Totes les reserves\n "
+            + "[3] Tancar Sessio\n "};
+        
+        String[] MenuSolicitarReserva = {"---------------\n SOL·LICITAR RESERVA \n---------------\n"
+            + "Introdueix '13' per tornar al menú\n "};
+        
+        String[] MenuModificarReserva = {"---------------\n MODIFICAR RESERVA \n---------------\n "
+            + "Introdueix 13 per cancel·lar la modificacio\n "};
+        
+        String[] MenuReservaPerMes = {"\n-------------------\n RESERVES PER MES \n-------------------\n "
+            + "Introdueix '13' per tornar al menú\n "};
+        
+        String[] MenuRegistrarUsuari = {"------------------\n REGISTRAR USUARI \n------------------\n "
+            + "Escriu 'adeu' per tornar al menu\n "};
+        
+        String[] MenuCancelarReserva = {"---------------\n CANCEL·LACIÓ RESERVA \n---------------\n"};
+        
+        String[] MenuBuscarReserva = {"-----------------\n BUSQUEDA RESERVA \n-----------------\n "
+            + "Introeudix '13' per tornar al menú\n "};
+        
+        String[] MenuRecuperarContrasenya= {"-----------------------\n RECUPERAR CONTRASENYA \n-----------------------\n "
+            + "Escriu 'adeu' per tornar al menu\n "};
+}
