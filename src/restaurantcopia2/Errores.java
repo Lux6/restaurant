8fae4ce7e76ec2c2451411595900b0626f082e3c
jerje <jerje@DESKTOP-E3BHB3L>
2017-03-20 17:58:55 +0100

    Modularitza't case 3 i creades noves class

---
+++ src/restaurantcopia2/Errores.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurantcopia2;
+
+/**
+ *
+ * @author jerje
+ */
+class Errores {
+    String sReservaNoTrobada="\033[31m" + "Reserva no trobada." + "\033[30m";
+}
