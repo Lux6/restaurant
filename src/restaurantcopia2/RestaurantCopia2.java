447793d20f6097e2bfab42a144834d96c30be793
jerje <jerje@DESKTOP-E3BHB3L>
2017-03-29 16:05:41 +0200

    Modularització horitzontal

--- src/restaurantcopia2/RestaurantCopia2.java
+++ src/restaurantcopia2/RestaurantCopia2.java
@@ -1,21 +1,25 @@
 package restaurantcopia2;
 
-import com.sun.org.apache.xml.internal.resolver.readers.TR9401CatalogReader;
 import java.util.Scanner;
-import java.sql.*;
-import static restaurantcopia2.restaurantFuncionsProcediments.*;
 
-public class RestaurantCopia2 extends restaurantFuncionsProcediments {
+class RestaurantCopia2 extends restaurantFuncionsProcediments {
 
     static final String JDBC_DRIVER = "org.postgresql.Driver";
     static final String DB_URL = "jdbc:postgresql://localhost:restaurant";
     static final String USER = "postgres";
     static final String PASS = "root";
 
-    public final void main(Menus Menus,Variables var,BaseDades db,Reserva res,TReserva reserva,Usuari usu) {
-                
+    public static void main (String[]args) {
+        //*Reservar espacio de clases*//
+        Scanner sc = new Scanner(System.in);
+        TReserva reserva = new TReserva();
+        Menus Menus = new Menus();
+        Variables var = new Variables();
+        BaseDades db = new BaseDades();
+        Usuari usu = new Usuari();
+        Reserva res = new Reserva();
+    
         while(true){
-            
             //Mostra el menu principal i llegeix opció del menu
             Menus(Menus.MenuPrincipal);
             var.iOpcioPrincipal = iLlegirOpcioMenu();
@@ -28,19 +32,19 @@ public class RestaurantCopia2 extends restaurantFuncionsProcediments {
             
             switch(var.iOpcioPrincipal){
                 
-                //************************************************************INICI SESSIÓ*****************************************************************//
+                //************************************************************INICI SESSIÓ*******************************************************************//
                 case 1:{ //
                     IniciSessio(Menus, var, db, res, reserva, usu);
                     
                     break;
                 }
-                //*************************************************************REGISTRE USUARI**********************************************************//
+                //**********************************************************REGISTRE USUARI******************************************************************//
                 case 2:{
                     RG_RegistreUsuari(var, Menus,db);
                     break;
                 }
                 
-                //*************************************************************RECUPERAR CONTRASENYA*************************************************//
+                //*******************************************************RECUPERAR CONTRASENYA*****************************************************************//
                 case 3:{
                     RecuperarContrasenya(var, Menus);
                     break;
